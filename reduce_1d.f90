889826cd8bcbac5402a170dd61f4303e02e0eecf
Olivia Hull <oliviahull0112@gmail.com>
2021-03-27 11:41:43 -0400

    renaming and shuffling

---
+++ reduce_1d.f90
@@ -0,0 +1,57 @@
+PROGRAM reduce_1d
+include 'mpif.h'
+  ! Goal of this program is to take an array, fill out task-specific data, and then send to task 1
+  ! e.g. say we have 3 mpi tasks, and task 1 has ints 0, 1; 2 has ints 2, 3; 3 has ints 4, 5
+  ! we want each task to make an array:
+  ! task 1: [0, 1, 0, 0, 0, 0]
+  ! task 2: [0, 0, 2, 3, 0, 0]
+  ! task 3: [0, 0, 0, 0, 4, 5]
+  ! and then send all data to task 1 so task 1 has:
+  ! task 1: [0, 1, 2, 3, 4, 5]
+
+  integer :: ntasks, my_id 
+  integer :: ierror, tag
+  integer :: bufsize
+  integer :: my_start
+  integer :: my_nvals = 2 ! number of ints each task gets
+  integer :: root = 0     ! send to mpi task 0
+  integer, allocatable :: my_data(:)    !
+  integer, allocatable :: send_buffer(:) ! buffer to fill and reduce
+  integer, allocatable :: rec_buffer(:)
+  
+  call MPI_INIT(ierror)
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, ntasks, ierror)
+  call MPI_COMM_RANK(MPI_COMM_WORLD, my_id,  ierror)
+
+  bufsize = my_nvals*ntasks
+  allocate(send_buffer(bufsize))
+  allocate(rec_buffer(bufsize))
+  send_buffer = 0
+  rec_buffer = 0
+  ! hold temp data:
+  allocate(my_data(my_nvals))
+  ! each task gets its own integers of data
+  do ii = 1, my_nvals
+    my_data(ii) = my_id*my_nvals+ii
+  enddo
+
+  ! place data in appropriate spot
+  my_start = my_id*my_nvals
+  do ii = 1, my_nvals
+    send_buffer(my_start+ii) = my_data(ii)
+  enddo
+
+  call MPI_REDUCE(send_buffer, rec_buffer, bufsize, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD, ierror)
+
+  if (my_id == root) then
+    write(*,*) "rec_buffer for root: ", rec_buffer
+  endif
+  if (my_id == 1) then
+    write(*,*) "rec_buffer for non-root: ", rec_buffer
+  endif
+
+  deallocate(send_buffer)
+  deallocate(my_data)
+
+call MPI_FINALIZE(ierror)
+END PROGRAM
---
+++ reduce_2d.f90
@@ -0,0 +1,114 @@
+PROGRAM reduce_2d
+include 'mpif.h'
+  ! Want to get fancier than the 1d array, and now generate *rows* of data
+  ! where the root task needs *columns* of data
+  ! In this case, say we have an array:
+  ! [ 0  1  2  3  4  5  6  7 
+  !   9 10 11 12 13 14 15 16
+  !  17 18 19 20 21 22 23 24 
+  !  25 26 27 28 29 30 31 32 ]
+  ! and each mpi task is assigned contiguous rows and round-robin columns
+  ! and each task has its rows, but needs its columns
+  ! so we gather the row data and send to the appropriate task for its columns
+  ! once this is working, it's the biggest piece of the puzzle
+  ! the next step will be to do this as a 3D array for extra staging
+
+  integer :: ntasks, my_id 
+  integer :: ierror, tag
+  integer :: bufsize
+  integer :: my_start
+  integer :: my_nvals = 2 ! number of ints each task gets ! THIS WILL LIKELY GO AWAY
+  integer :: root = 0     ! send to mpi task 0
+
+  integer :: ii, jj
+  integer :: nrows = 8    ! maybe change to 4 later for easier matrix size
+  integer :: ncols = 8
+  integer :: n_myrows, n_mycols, mycols, myrows
+  integer, allocatable :: tot_data(:,:) ! cheating...just to pick my_data from
+  integer, allocatable :: my_data(:)    !
+  integer, allocatable :: send_buffer(:) ! buffer to fill and reduce
+  integer, allocatable :: rec_buffer(:)
+  
+  call MPI_INIT(ierror)
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, ntasks, ierror)
+  call MPI_COMM_RANK(MPI_COMM_WORLD, my_id,  ierror)
+
+  ! generate total data:
+  allocate(tot_data(nrows, ncols))
+  do ii = 1, nrows
+    do jj = 1, ncols
+      tot_data(ii, jj) = ii*jj
+    enddo
+  enddo
+
+  if (my_id == root) then
+    do ii = 1, nrows
+      write(*,*) (tot_data(ii, jj), jj=1,ncols)
+    enddo
+  endif
+
+  _REDUCE(sbuf, rbuf, count, stype, op, root, comm, ierr)! generate total data:
+  allocate(tot_data(nrows, ncols))
+  do ii = 1, nrows
+    do jj = 1, ncols
+      tot_data(ii, jj) = ii*jj
+    enddo
+  enddo
+
+  if (my_id == root) then
+    do ii = 1, nrows
+      write(*,*) (tot_data(ii, jj), jj=1,ncols)
+    enddo
+  endif
+  ! determine who gets which rows (contiguous):
+  ! NOTE: I am just going to be doing this with even divisibility first
+  !       so I will need to come back and check that the remainder feature actually works
+  n_myrows = nrows / ntasks
+  diff = ntasks - (n_myrows * ntasks)
+  myrow_start = n_myrows*my_id  ! remember to account for the non-zero indexing somewhere
+  if (my_id == ntasks - 1) then
+    n_myrows = n_myrows+diff ! give the leftovers to the last task
+  endif  
+
+  ! determine who gets which columns (contiguous for now, then round-robin):
+  ! note that doing contiguous for now just saves the staging step
+  ! We can add the staging step in once everything else is working
+
+
+
+  ! Now, give each task their row data:
+
+
+
+!  bufsize = my_nvals*ntasks
+!  allocate(send_buffer(bufsize))
+!  allocate(rec_buffer(bufsize))
+!  send_buffer = 0
+!  rec_buffer = 0
+!  ! hold temp data:
+!  allocate(my_data(my_nvals))
+!  ! each task gets its own integers of data
+!  do ii = 1, my_nvals
+!    my_data(ii) = my_id*my_nvals+ii
+!  enddo
+!
+!  ! place data in appropriate spot
+!  my_start = my_id*my_nvals
+!  do ii = 1, my_nvals
+!    send_buffer(my_start+ii) = my_data(ii)
+!  enddo
+!
+!  call MPI_REDUCE(send_buffer, rec_buffer, bufsize, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD, ierror)
+!
+!  if (my_id == root) then
+!    write(*,*) "rec_buffer for root: ", rec_buffer
+!  endif
+!  if (my_id == 1) then
+!    write(*,*) "rec_buffer for non-root: ", rec_buffer
+!  endif
+!
+!  deallocate(send_buffer)
+!  deallocate(my_data)
+
+call MPI_FINALIZE(ierror)
+END PROGRAM
